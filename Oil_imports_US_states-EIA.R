fa49d9c06c26a843558c53a51828aaa1985faa3e
Eric Youngson <eayoungs@gmail.com>
2017-09-28 21:09:56 -0700

    Added multilevel sankey example in .HTML, modifed sankeyPollutantsImportsEIA function to coordiante facility names between levels

--- Oil_imports_US_states-EIA.R
+++ Oil_imports_US_states-EIA.R
@@ -102,15 +102,55 @@ sankeyRefineryImportsEIA <- function(csv_file_in){
 
     # summarise data
     importsSankey = sqldf("SELECT origin_var, destination_var, dat_var AS BARRELS FROM imports
-                          WHERE NOT origin_var='World' AND years_var='2009' AND destination_var IN ('CHEVRON USA / RICHMOND / CA',
+                          WHERE NOT origin_var='World' AND years_var='2009' AND destination_var IN
+                          ('CHEVRON USA / RICHMOND / CA',
                           'PHILLIPS 66 CO / SAN FRANCISCO / CA',
                           'VALERO REFINING CO CALIFORNIA / BENICIA / CA',
                           'SHELL OIL PRODUCTS US / MARTINEZ / CA',
                           'TESORO CORP / GOLDEN EAGLE / CA') GROUP BY 1,2")
-    #                    UNION ALL
-    #                      SELECT ORGANIZATION, (TEAM_CITY||' '||TEAM_NAME) AS NBA_TEAM, COUNT(PLAYER_NAME) AS PLAYERS
-    #                      FROM draft
-    #                      GROUP BY 1,2")
+
+    # Sankey diagram using googleVis
+    plot(gvisSankey(importsSankey, from="origin_var", to="destination_var", weight="dat_var",
+        options=list(height=800, width=850,
+        sankey="{
+          link:{color:{fill: 'lightgray', fillOpacity: 0.7}},
+          node:{nodePadding: 5, label:{fontSize: 12}, interactivity: true, width: 20},
+        }")
+      )
+    )
+}
+
+
+sankeyPollutantsImportsEIA <- function(csv_file_in){
+    # Description:
+    #   
+    # 
+    # Args:
+    #   
+    # 
+    # Returns:
+    #   
+   
+    ref_pol_08 = read.csv("data/chevron_cap-08.csv", stringsAsFactors = FALSE)
+    ref_pol_08[is.na(ref_pol_08)] <- 0
+
+    # Rename all the facility emissions sources to align with center level of sankey chart 
+    facilityNames = list('CHEVRON' = 'CHEVRON USA / RICHMOND / CA',
+                         'PHILLIPS' = 'PHILLIPS 66 CO / SAN FRANCISCO / CA',
+                         'VALERO' = 'VALERO REFINING CO CALIFORNIA / BENICIA / CA',
+                         'SHELL' = 'SHELL OIL PRODUCTS US / MARTINEZ / CA',
+                         'TESORO' = 'TESORO CORP / GOLDEN EAGLE / CA')
+
+    for (name in names(facilityNames)) {
+        ref_pol_08[grep(name, ref_pol_08$FNAME),]$FNAME = facilityNames[[name]]
+
+    #    faciltyPols = sqldf(sprintf("SELECT sum(TOGT), sum(ROGT) , sum(COT), sum(SOXT), sum(PMT), sum(PM10T) FROM ref_pol_08
+    #                                 WHERE FNAME = '%s'"), facilityNames[[name]])
+    }
+
+    # summarise data
+    importsSankey = sqldf(sprintf("SELECT origin_var, destination_var, dat_var AS BARRELS FROM imports
+                          WHERE FNAME IN '%s'"), facilityNames)
 
 
     # Sankey diagram using googleVis
